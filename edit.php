2ddfb351c1bc05ab8f0bffc47f22c259bca24458
trashkaloff <trashkaloff@gmail.com>
2017-02-26 22:05:52 +0200

    start updete ->SELECT WHERE id->form

--- edit.php
+++ edit.php
@@ -15,3 +15,16 @@ $pdo_statement->bindValue(":created_at", $_POST['created_at']);
 $pdo_statement->bindValue(":id", $_GET['id']);
 $pdo_statement->execute();
 
+
+$ID = $_GET['id'];
+$sql = ('SELECT * FROM article WHERE id = :id');
+$pdo_statement = $pdo_conn->prepare($sql);
+$pdo_statement->execute(array(':id' => $ID));
+$result = $pdo_statement->fetch();
+?>
+<form method="post" action="index.php">
+    <input type="text" name="name" placeholder="name">
+    <input type="text" name="description" placeholder="description">
+    <input type="text" name="created_at" placeholder="created_at">
+    <input type="submit">
+</form>
