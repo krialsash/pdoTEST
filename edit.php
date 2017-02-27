763cd0449c013f27491cd619745e43e8c93d0022
trashkaloff <trashkaloff@gmail.com>
2017-02-28 01:18:35 +0200

    article file

--- edit.php
+++ edit.php
@@ -3,18 +3,18 @@ require_once 'conn.php';
 
 // select * from article Where id
 // -> form
-
-$sql = "UPDATE article SET name = :name, 
+if (empty($_POST["name"]) || empty($_POST["description"]) || empty($_POST["created_at"])) {
+    $sql = "UPDATE article SET name = :name, 
 description = :description,
  created_at = :created_at 
  WHERE id = :id";
-$pdo_statement = $pdo_conn->prepare($sql);
-$pdo_statement->bindValue(":name", $_POST['name']);
-$pdo_statement->bindValue(":description", $_POST['description']);
-$pdo_statement->bindValue(":created_at", $_POST['created_at']);
-$pdo_statement->bindValue(":id", $_GET['id']);
-$pdo_statement->execute();
-
+    $pdo_statement = $pdo_conn->prepare($sql);
+    $pdo_statement->bindValue(":name", $_POST['name']);
+    $pdo_statement->bindValue(":description", $_POST['description']);
+    $pdo_statement->bindValue(":created_at", $_POST['created_at']);
+    $pdo_statement->bindValue(":id", $_GET['id']);
+    $pdo_statement->execute();
+}
 
 $ID = $_GET['id'];
 $sql = ('SELECT * FROM article WHERE id = :id');
