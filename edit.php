7d2ffe972c31a48e5478cc2a5f34b17b9b597be4
trashkaloff <trashkaloff@gmail.com>
2017-03-03 16:36:03 +0200

    article file

--- edit.php
+++ edit.php
@@ -3,41 +3,37 @@ require_once 'conn.php';
 
 // select * from article Where id
 // -> form
-if (!empty($_POST['update'])){
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $description = $_POST['description'];
-    $created_at = $_POST['created_at'];
-    $pdo_statement = $pdo_conn->prepare("UPDATE article SET name = :name, description = :description, 
-              created_at = :created_at  WHERE id= :id" );
+if (!isset($_POST['name']) && !isset($_POST['description']) && !isset($_POST['created_at'])){
+    $sql = "UPDATE article SET name = :name, 
+description = :description,
+ created_at = :created_at 
+ WHERE id = :id";
+    $pdo_statement = $pdo_conn->prepare($sql);
     $pdo_statement->bindValue(":name", $_POST['name']);
     $pdo_statement->bindValue(":description", $_POST['description']);
     $pdo_statement->bindValue(":created_at", $_POST['created_at']);
-    $pdo_statement->bindValue(":id", $_POST['id']);
-    $result = $pdo_statement->execute();
-    if($result) {
-        header('location:index.php');
-    }
+    $pdo_statement->bindValue(":id", $_GET['id']);
+    $pdo_statement->execute();
 }
-$sql = 'SELECT * FROM article WHERE id =' ;
-$id = $_GET['id'];
-//$name = $_POST['name'];
-//$description = $_POST['description'];
-//$created_at = $_POST['created_at'];
+
+$ID = $_POST['id'];
+$sql = ('SELECT * FROM article WHERE id = :id');
 $pdo_statement = $pdo_conn->prepare($sql);
-$pdo_statement->execute();
-$result = $pdo_statement->fetchAll();
-
+$pdo_statement->execute(array(':id' => $ID));
+$result = $pdo_statement->fetch();
+foreach($result as $row){
+    $row['name'];
+    $row['description'];
+    $row['created_at'];
+}
 ?>
-
-<form method="post" action="edit.php">
-    <input type="text" name="name" placeholder="name" id="name" value="<?php echo $name;?>">
-    <input type="text" name="description" placeholder="description" id="description" value="<?php echo $description;?>" >
-    <input type="text" name="created_at" placeholder="created_at" id="created_at" value="<?php echo $created_at;?>">
-    <input type="hidden" name="id" value="<?php echo $_GET['id'];?>">
-    <input type="submit" id="update"  >
-</form>
-
+    <form method="post" action="edit.php">
+        <input type="text" name="name" placeholder="name"  value="<?php echo $row['name'];?>">
+        <input type="text" name="description" placeholder="description"  value="<?php echo  $row['description'];?>">
+        <input type="text" name="created_at" placeholder="created_at"  value="<?php echo $row['created_at'];?>">
+        <input type="hidden" name="id" value=<?php echo $_GET['id'];?>>
+        <input type="submit" name="update" value="Update" >
+    </form>
 <?php
 if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['created_at'])) {
     $sql = "UPDATE article SET name = :name, 
